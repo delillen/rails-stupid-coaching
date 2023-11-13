79229480be032d9c698714217db242571592605f
Nicolas Delille <delille.n@gmail.com>
2023-11-13 12:55:27 +0100

    OK

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,9 @@
 class QuestionsController < ApplicationController
 
   def ask
-
   end
 
   def answer
-    raise
-
     @answer = ""
 
     if params[:question]
@@ -14,15 +11,13 @@ class QuestionsController < ApplicationController
 
       if @question.include? "?"
         @answer = "Silly question, get dressed and go to work!"
-      else if @question == "I am going to work"
+      elsif @question == "I am going to work"
         @answer = "Great!"
       else
         @answer = "I don't care, get dressed and go to work!"
       end
-
     end
 
-    @answer
   end
 
 end
